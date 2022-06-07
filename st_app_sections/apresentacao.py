8368f9803f9f70e34658504a70287d9e45835baf
Arthur Lins <arthurlins@id.uff.br>
2022-06-07 12:21:08 -0300

    vf

--- st_app_sections/apresentacao.py
+++ st_app_sections/apresentacao.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 __all__ = ["apresentacao"]
 def apresentacao():
-    st.write('Bem vindo ao TFT Tracker')
+    st.header('Bem vindo ao TFT Tracker')
     st.write('Na aba Tracker você poderá acompanhar a evolução diária de qualquer player de qualquer região incluindo você próprio. ')
     st.write('A ideia desta ferramenta é descobrir pessoas que estão jogando bem o patch e analisar suas estatisticas através de sites como o lolchess e idenficar como e porque eles estão sendo bem sucedidos.')
     st.write('Na aba Tracker, você podrá escolher uma região e atualizar em tempo real a evolução de pdls das pessoas junto a ladder atual.')
